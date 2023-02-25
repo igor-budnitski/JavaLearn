c6f490943518874f09f37a6cb1c732037c538435
igor-budnitski <96424046+igor-budnitski@users.noreply.github.com>
2023-02-25 12:45:23 +0200

    25/2 - commit

---
+++ src/Date/DateJava.java
@@ -0,0 +1,11 @@
+package Date;
+
+import java.util.Date;
+
+public class DateJava {
+    public static void main(String[] args) {
+        Date date = new Date(88,1,1);
+        System.out.println(date);
+        System.out.println(date.getDay());
+    }
+}
